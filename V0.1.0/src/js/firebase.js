46e7898da8326ebc74ff8d391183a9fe062a9481
elodieGeorgelin <elodiegeorgelin@yahoo.fr>
2018-11-07 16:38:58 +0100

    Implémentation de la fonction createAccount(userName, mail, password)

--- V0.1.0/src/js/firebase.js
+++ V0.1.0/src/js/firebase.js
@@ -9,8 +9,22 @@ var config = {
 };
 firebase.initializeApp(config);
 
-function createAccount(userName, mail, password) {
-
+function createAccount(userName, mail, password){
+  firebase
+  .auth()
+  .createUserWithEmailAndPassword(mail, password)
+  .then(function() {
+    var rootRef = firebase.database().ref();
+    var usersRef = rootRef.child('users');
+    var user = {userName: userName, mail: mail};
+    return usersRef.child(firebase.auth().currentUser.uid).set(user);
+  })
+  .then(function() {
+    window.location.href = 'index.html';
+  })
+  .catch(function(error) {
+    console.log(error.message);
+  });
 }
 
 function logIn(mail, password){
