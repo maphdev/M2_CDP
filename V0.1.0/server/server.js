5d51b8f5dcf7d034353c3c2174470c4bd4b3fa40
Manon Philippot <manon.philippot@gmail.com>
2018-11-07 15:48:39 +0100

    server & archi

--- V0.1.0/server/server.js
+++ V0.1.0/server/server.js
@@ -3,9 +3,37 @@ var express = require('express')
 var path = require("path");
 var app = express()
 
+// import ressources
+app.use(express.static(path.join(__dirname, '../src')));
+
 //Define request response in root URL (/)
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../src', 'index.html'));
+  res.redirect('/index.html');
+})
+
+app.get('/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/html', 'index.html'));
+})
+
+app.get('/createAccount.html', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/html', 'createAccount.html'));
+})
+
+app.get('/login.html', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/html', 'login.html'));
+})
+
+app.get('/createProject.html', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/html', 'createProject.html'));
+})
+
+app.get('/project.html', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/html', 'project.html'));
+})
+
+app.get('/settings.html', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/html', 'settings.html'));
 })
 
 //Launch listening server on port 8080
