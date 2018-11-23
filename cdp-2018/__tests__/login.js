a63d5a28fa34a447b6fc197ffbf2c285afb81bb3
elodieGeorgelin <elodiegeorgelin@yahoo.fr>
2018-11-23 10:25:33 +0100

    update login.js

--- cdp-2018/__tests__/login.js
+++ cdp-2018/__tests__/login.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer');
+var firebase = require("../database/authConfig.js").firebase;
 
 const baseURL = 'http://localhost:8080';
 const loginRoute = `${baseURL}/login`;
 const user = {
     username : 'Util1',
     mail : 'Util1@mail.com',
-    password : 'Util01'
+    password : 'Util00'
 }
 
 let browser
@@ -16,25 +17,26 @@ beforeAll(async () => {
       process.env.DEBUG
         ? {
             headless: false,
-            slowMo: 50,
+            //slowMo: 10,
           }
         : {}
     )
-    page = await browser.newPage()
+    page = await browser.newPage();
   })
 
 describe('Login', () => {
     test('Login', async () => {
-        await page.goto(baseURL)
-        await page.click("#menu-connect")
-        await page.type('#form_email', user.mail)
-        await page.type('#form_password', user.password)
-        await page.click("#form_button")
+        await page.goto(baseURL);
+        await page.waitFor('#menu-connect');
+        await page.click("#menu-connect");
+        await page.type("#form_email", user.mail);
+        await page.type("#form_password", user.password);
+        await page.waitFor('#form_button');
+        await page.click("#form_button");
     })
 })
 
+
 afterAll(() => {
-    if (!process.env.DEBUG) {
-      browser.close()
-    }
+      browser.close();
   })
