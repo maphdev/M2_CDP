09c71e0c7aa4428882f6b3950d07a4f9d921ef78
Manon Philippot <manon.philippot@gmail.com>
2018-11-23 16:01:04 +0100

    Dev list in project settings

--- cdp-2018/routes/project.js
+++ cdp-2018/routes/project.js
@@ -10,15 +10,13 @@ router.route('/createProject')
     res.render('pages/createProject');
   })
   .post(async function(req, res) {
-    console.log('post');
-
     await cf.mongoose.connect(cf.dbURL);
     var project = new cf.Project();
     project.name = req.body.name;
     project.description = req.body.description;
     project.durationSprint = req.body.durationSprint;
     project.startingDay = req.body.startingDay;
-    project.devList = req.body.dev;
+    project.devList = req.session.userMail;
     await project.save(function(err) {
       if (err) {
         res.send({status: 500, error: err});
@@ -72,9 +70,75 @@ router.route('/settings')
       if (err) {
         res.json({status: 500, error: err});
       }
-      res.redirect('project');
+      res.redirect('/settings');
       cf.mongoose.disconnect();
     });
   });
+
+router.route('/addDevToProject')
+  .post(async function(req, res) {
+    console.log(req.body.devMail);
+
+    let projectId = req.session.projectId;
+    let devMail = req.body.devMail;
+
+    await cf.mongoose.connect(cf.dbURL);
+
+    // the dev exist ?
+    await cf.User.count({mail: devMail}, function(err, count) {
+      // yes the dev exists
+      if (count > 0){
+        // the dev is in the Project's dev's list ?
+        (function() {
+          cf.Project.findOne({_id: projectId}, function(err, result) {
+            if (err) {
+              res.json({status: 500, error: err});
+            }
+
+            for (var i = 0; i < result['devList'].length; i++){
+              // yes the dev is already in here, we stop
+              if (result['devList'][i] == devMail){
+                res.render('pages/settings', {project: result, error: "L'adresse email est déjà dans la liste des développeurs !"});
+                return;
+              }
+            }
+            // no the dev is not in here, we add him
+            (function() {
+              cf.Project.findOneAndUpdate({_id: projectId}, {$push:{'devList': devMail}}, {upsert:true}, function(err, project) {
+                if (err) {
+                  res.json({status: 500, error: err});
+                }
+                res.redirect('/settings');
+              });
+            })();
+          });
+        })();
+      // no the dev doesn't exist
+      } else {
+        (function() {
+          cf.Project.findOne({_id: projectId}, function(err, project) {
+            if (err) {
+              res.json({status: 500, error: err});
+            }
+            res.render('pages/settings', {project: project, error: "L'adresse email ne correspond à aucun compte développeur !"});
+          });
+        })();
+      }
+    });
+  });
+
+  router.route('/deleteDevFromProject')
+    .post(async function(req, res) {
+      let projectId = req.session.projectId;
+      let devMail = req.body.id;
+      await cf.mongoose.connect(cf.dbURL);
+      await cf.Project.update({_id: projectId}, {$pull:{'devList': devMail}}, function(err, project) {
+        if (err) {
+          res.json({status: 500, error: err});
+        }
+        res.redirect('/settings');
+        cf.mongoose.disconnect();
+      });
+    });
 
 module.exports = router;
